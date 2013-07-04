e12f457c971efed426c3bcda7d04e3ab6cae4bc2
pranavbhole <pranavbhole@gmail.com>
2013-07-03 22:49:07 -0500

    first commit

---
+++ Arrays/src/Combinations.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+public class Combinations {
+public static int a[]={2,4,5,1,1,3,5,8};
+static void combinations(ArrayList<Integer> temp,int start, int count, int r){
+if(count==r){
+	for(int i=0; i<temp.size();i++)
+	System.out.print(temp.get(i)+" ");
+	System.out.println();
+}
+else{
+
+	
+for(int i=start;i<a.length && temp.size()<=r;i++){
+	temp.add(count,new Integer(a[i]));
+	combinations(temp,i, count+1, r);
+	temp.remove(count);	
+}
+}
+
+}
+
+public static void main(String args[]){
+	ArrayList<Integer> temp=new ArrayList<Integer>();
+	combinations(temp,0,0,2);
+}
+}
