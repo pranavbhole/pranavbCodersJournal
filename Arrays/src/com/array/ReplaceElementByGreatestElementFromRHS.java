729931ccf3553bf1fd01c0611da1f32f1fde2f4a
pranavbhole <pranavbhole@gmail.com>
2013-08-04 04:54:47 -0500

    adding leaders

--- Arrays/src/com/array/ReplaceElementByGreatestElementFromRHS.java
+++ Arrays/src/com/array/ReplaceElementByGreatestElementFromRHS.java
@@ -1,7 +1,10 @@
 package com.array;
 
+import java.util.Stack;
+
 public class ReplaceElementByGreatestElementFromRHS {
 int a[];
+Stack<String> stackOfLeaders=new Stack<String>();
 	public ReplaceElementByGreatestElementFromRHS() {
 		a=new int[10];
 		for(int i=0; i<10; i++)
@@ -11,19 +14,26 @@ int a[];
 	void replaceElementByNextGreatest(){
 		int greatestTillNow;
 		greatestTillNow=a[a.length-1];
+		stackOfLeaders.push(""+greatestTillNow);
 		a[a.length-1]=-1;
 		for(int i=a.length-2; i>=0; i--)
 		{
 		int temp=a[i];
 		a[i]=greatestTillNow;
-		if(temp>greatestTillNow)greatestTillNow=temp;
+		if(temp>greatestTillNow){greatestTillNow=temp;
+		stackOfLeaders.push(""+greatestTillNow);	
+		}
 		}
 	print();
 	}
 	
 	 void print() {
 			for(int i=0; i<a.length; i++)System.out.print(a[i]+" , ");
-			System.out.println();		
+			System.out.println("");
+			if(!stackOfLeaders.isEmpty())System.out.println("Leaders :");
+			while(!stackOfLeaders.isEmpty()){
+				System.out.print(" "+stackOfLeaders.pop());
+			}
 	}
 	/**
 	 * @param args
